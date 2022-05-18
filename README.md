359c4b96894e82c0f4b2d223039ad9d260790f3e
허혁 <hhur@duranno.com>
2022-05-18 10:38:44 +0900

    readme 파일 수정 ,src > hhtest.js  생성

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 reactMasterClass 안의 프로젝트는 https://nodejs.org/ko/ 에서 LTS 버전을 설치하시고
 /reactMasterClass/react-style-components/ 경로에서 npm i 하시고 npm start 하시면
 로컬에서 보실 수 있습니다 :D
+
+허혁 수정..
---
+++ reactMasterClass/react-style-components/src/hhtest.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+
+const darkTheme = {
+  textColor: 'whitesmoke',
+  bgColor: '#111',
+}
+const lightTheme = {
+  textColor: '#111',
+  bgColor: 'whitesmoke',
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
